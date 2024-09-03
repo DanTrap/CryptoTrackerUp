021c1ce2756333f1ccb4c448961e513530dcc8c6
dantrapdev <dantrapdev@gmail.com>
2024-09-03 16:36:10 +0300

    Setup dependency injection and coin list components

--- app/src/main/kotlin/com/dantrap/cryptotracker/DI.kt
+++ app/src/main/kotlin/com/dantrap/cryptotracker/DI.kt
@@ -1,10 +1,11 @@
 package com.dantrap.cryptotracker
 
 import ru.mobileup.template.core.coreModule
+import ru.mobileup.template.features.coins.coinsModule
 import ru.mobileup.template.features.pokemons.pokemonsModule
 
 val allModules = listOf(
     coreModule(BuildConfig.BACKEND_URL, BuildConfig.API_KEY),
-    pokemonsModule
+    pokemonsModule,
     coinsModule
 )
