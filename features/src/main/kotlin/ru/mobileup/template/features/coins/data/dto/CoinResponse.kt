bc867f09b9062a6b67372a32a61f291ce28bdada
dantrapdev <dantrapdev@gmail.com>
2024-09-03 16:27:29 +0300

    Create CoinApi and dto model

---
+++ features/src/main/kotlin/ru/mobileup/template/features/coins/data/dto/CoinResponse.kt
@@ -0,0 +1,30 @@
+package ru.mobileup.template.features.coins.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import ru.mobileup.template.features.coins.domain.Coin
+
+@Serializable
+data class CoinResponse(
+    @SerialName("current_price")
+    val currentPrice: Double,
+    @SerialName("id")
+    val id: String,
+    @SerialName("image")
+    val image: String,
+    @SerialName("name")
+    val name: String,
+    @SerialName("price_change_percentage_24h")
+    val priceChangePercentage24h: Double,
+    @SerialName("symbol")
+    val symbol: String,
+)
+
+internal fun CoinResponse.toDomain(): Coin = Coin(
+    id = id,
+    name = name,
+    symbol = symbol.uppercase(),
+    image = image,
+    currentPrice = currentPrice,
+    priceChangePercentage24h = priceChangePercentage24h
+)
