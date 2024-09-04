eaa91f50c578a53b4ffcfbf96177db1a25197e8a
dantrapdev <dantrapdev@gmail.com>
2024-09-04 12:50:00 +0300

    Create fake and real implementations of CoinDetailsComponent

---
+++ features/src/main/kotlin/ru/mobileup/template/features/coins/presentation/details/RealCoinDetailsComponent.kt
@@ -0,0 +1,23 @@
+package ru.mobileup.template.features.coins.presentation.details
+
+import com.arkivanov.decompose.ComponentContext
+import me.aartikov.replica.algebra.normal.withKey
+import ru.mobileup.template.core.error_handling.ErrorHandler
+import ru.mobileup.template.core.utils.observe
+import ru.mobileup.template.features.coins.data.CoinRepository
+
+class RealCoinDetailsComponent(
+    coinId: String,
+    componentContext: ComponentContext,
+    coinRepository: CoinRepository,
+    errorHandler: ErrorHandler,
+) : ComponentContext by componentContext, CoinDetailsComponent {
+
+    private val coinDetailsReplica = coinRepository.coinDetailsById.withKey(coinId)
+
+    override val coinDetailsState = coinDetailsReplica.observe(this, errorHandler)
+
+    override fun onRetryClick() = coinDetailsReplica.refresh()
+
+    override fun onRefresh() = coinDetailsReplica.refresh()
+}
